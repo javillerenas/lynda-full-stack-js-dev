fac658d97a7c1851d172faf0652f8f224965583e
Javier Llerenas <javr.llerenas@gmail.com>
2020-02-10 13:51:09 -0800

    chore(setup): setup config for project (#1)

---
+++ server.js
@@ -0,0 +1,12 @@
+import http from 'http';
+
+const server = http.createServer();
+server.listen(8080)
+
+server.on('request', (req, res) => {
+  res.write('Hello HTTP!\n');
+  setTimeout(() => {
+    res.write('I can stream!\n')
+    res.end()
+  }, 2000);
+});
