3ac7ad1e428e13538a5e534fe3e8a45069e3bb70
Javier Llerenas <javr.llerenas@gmail.com>
2020-02-13 12:57:19 -0800

    finish module 2 (#2)

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Header = ({ message }) => {
+  return <h2 className="Header text-center">{message}</h2>;
+};
+
+Header.propTypes = {
+  message: PropTypes.string
+};
+Header.defaultProps = {
+  message: "This is a header"
+};
+
+export default Header;
